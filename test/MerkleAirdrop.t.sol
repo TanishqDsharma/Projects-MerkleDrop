053e7582accbcef1c6a399640ee0a0b8e8e04c32
TanishqDsharma <tanishqsharma612@gmail.com>
2024-10-21 09:52:25 +0000

    Testing Setup done

---
+++ test/MerkleAirdrop.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.16;
+
+import {Test} from "../lib/forge-std/src/Test.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+
+contract MerkleAirdropTest is Test{
+    function setUp() external {
+
+    }
+} 
